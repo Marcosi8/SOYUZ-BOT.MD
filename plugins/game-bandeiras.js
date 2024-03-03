55c0658fe54c70843df1f67419b8fb6b9a76ddb4
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-03-03 18:26:04 -0300

    Create game-bandeiras.js

---
+++ plugins/game-bandeiras.js
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let te = `
+🌍 *Adivinhe a Bandeira do País:* 
+    
+*📌 Exemplo de Uso:* _${usedPrefix+command}_
+`
+    let id = m.chat;
+    
+    if (id in conn.flagsGame) {
+        return conn.reply(m.chat, `⚠️ O jogo de bandeiras já está em andamento!`, conn.flagsGame[id][0]);
+    }
+    
+    let { countryCode, flagUrl, correctAnswer } = await getFlag();
+    conn.flagsGame[id] = [
+        await conn.sendFile(m.chat, flagUrl, 'flag.png', `🚩 Qual é o país desta bandeira?`, m),
+        correctAnswer
+    ];
+};
+
+handler.help = ['bandeira'];
+handler.tags = ['game'];
+handler.command = ['bandeira', 'adivinha', 'guess'];
+
+async function getFlag() {
+    const response = await fetch('https://flagcdn.com/pt/codes.json');
+    const data = await response.json();
+    const countries = Object.keys(data);
+    const randomCountryCode = countries[Math.floor(Math.random() * countries.length)];
+    const flagUrl = `https://flagcdn.com/256x192/${randomCountryCode}.png`;
+    const correctAnswer = data[randomCountryCode];
+    return { countryCode: randomCountryCode, flagUrl, correctAnswer };
+}
+
+export default handler;
