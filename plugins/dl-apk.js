a96bfaa326d55afebb28d382be2f722aa0105df6
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-03-03 01:53:00 -0300

    Update dl-apk.js

--- plugins/dl-apk.js
+++ plugins/dl-apk.js
@@ -3,9 +3,11 @@ import { download } from 'aptoide-scraper';
 let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   try {
     if (['modapk', 'apk', 'app'].includes(command)) {
-      if (!text) throw `*[❗] Forneça o nome do APK que você deseja baixar!.*`;
-      m.react(rwait)
+      if (!text) throw `*[❗] Forneça o nome do APK que você deseja baixar!*`;
+      
+      m.react(rwait);
       await conn.reply(m.chat, global.wait, m);
+      
       let data = await download(text);
 
       if (data.size.replace(' MB', '') > 200) {
@@ -16,19 +18,21 @@ let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
         return await conn.sendMessage(m.chat, { text: '*[⛔] O arquivo é muito pesado.*' }, { quoted: m });
       }
 
-      await conn.sendMessage(
+      await conn.sendFile(
         m.chat,
-        { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null },
-        { quoted: m }
-      )
+        data.dllink,
+        data.name + '.apk',
+        `*Nome do APK:* ${data.name}\n*Tamanho:* ${data.size}\n*Versão:* ${data.version}\n*Ícone:* ${data.icon}`,
+        m
+      );
     }
-    m.react(done)
+    m.react(done);
   } catch {
     throw `*[🪩] Certifique-se de fornecer um nome/link válido.*`;
   }
 };
 
-handler.help = ['apk', 'app']
-handler.tags = ['dl', 'prime']
+handler.help = ['apk', 'app'];
+handler.tags = ['dl', 'prime'];
 handler.command = ['modapk', 'apk', 'app'];
 export default handler;
