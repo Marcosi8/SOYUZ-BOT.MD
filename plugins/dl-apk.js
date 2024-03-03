4a2acf5d8f837e220fed3cc2a764ee811a6159a9
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-03-03 01:30:44 -0300

    Update dl-apk.js

--- plugins/dl-apk.js
+++ plugins/dl-apk.js
@@ -1,12 +1,14 @@
-import { download } from 'aptoide-scraper';
+import { download, appInfo } from 'aptoide-scraper'; // Importar a função appInfo
 
 let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   try {
-    if (command === 'modapk', 'apk', 'app') {
-      if (!text) throw `*[❗] Forneça o nome do APK que você deseja baixar!.*`;
-      m.react(rwait)
+    if (command === 'modapk' || command === 'apk' || command === 'app') { // Corrigindo a condição if
+      if (!text) throw `*[❗] Forneça o nome do APK que você deseja baixar!*`;
+      m.react(rwait);
       await conn.reply(m.chat, global.wait, m);
+
       let data = await download(text);
+      let info = await appInfo(text); // Obter informações sobre o APK
 
       if (data.size.replace(' MB', '') > 200) {
         return await conn.sendMessage(m.chat, { text: '*[⛔] O arquivo é muito grande.*' }, { quoted: m });
@@ -18,17 +20,22 @@ let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
 
       await conn.sendMessage(
         m.chat,
-        { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null },
+        {
+          document: { url: data.dllink },
+          mimetype: 'application/vnd.android.package-archive',
+          fileName: data.name + '.apk',
+          caption: `*Nome do APK:* ${info.name}\n*Pacote:* ${info.package}\n*Versão:* ${info.version}\n*Ícone:* ${info.icon}` // Adicionar informações sobre o APK na legenda
+        },
         { quoted: m }
-      )
+      );
     }
-  m.react(done)
+    m.react(done);
   } catch {
     throw `*[🪩] Certifique-se de fornecer um nome/link válido.*`;
   }
 };
 
-handler.help = ['apk', 'app']
-handler.tags = ['dl', 'prime']
+handler.help = ['apk', 'app'];
+handler.tags = ['dl', 'prime'];
 handler.command = ['modapk', 'apk', 'app'];
 export default handler;
