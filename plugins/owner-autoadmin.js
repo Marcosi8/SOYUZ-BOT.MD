9ce84f3357b2c57f2b6573d662c6988f30697959
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-03-08 22:52:39 -0300

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -2,7 +2,9 @@ let handler = async (m, { conn, isAdmin }) => {
   if (isAdmin) throw `*O proprietário já é adm!*`
   await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 }
-handler.command = /^admin|adm|autoadmin$/i
+handler.help = ['autoadm']
+handler.tags = ['owner']
+handler.command = /^admin|adm|autoadm$/i
 handler.rowner = true
 handler.botAdmin = true
 export default handler
